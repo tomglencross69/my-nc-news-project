7ef967017805d4877fc847cc19c5d64a140d16dd
Tom Glencross <tomglencross@Toms-MBP-2.northcoders.com>
2024-11-28 12:33:26 +0000

    completes task 13 comment count

--- sql-test-pad.sql
+++ sql-test-pad.sql
@@ -69,7 +69,6 @@ SELECT articles.article_id, articles.title, articles.topic, articles.author, art
 FROM articles
 LEFT JOIN comments
 ON articles.article_id = comments.article_id
-WHERE articles.article_id = 1
 GROUP BY 
 articles.article_id
 ORDER BY articles.created_at DESC;
